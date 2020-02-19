dc79735f9a539bd6c944d143388efe4a2b107e3d
Rui2450 <2450782689@qq.com>
2020-02-19 17:28:28 +0800

    加入了多线程，更快的签到

--- src/main/java/com/rui/Signin.java
+++ src/main/java/com/rui/Signin.java
@@ -1,6 +1,7 @@
 package com.rui;
 
 import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
@@ -25,6 +26,7 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.CompletionService;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -47,28 +49,28 @@ import java.util.regex.Pattern;
 public class Signin {
     public static final Pattern TBS = Pattern.compile(".tbs.:\\s*\"\\S*");
     public static final String TIEBA_SEED_URL = "http://tieba.baidu.com/f/like/mylike?pn=1";
-    LinkedHashMap<String, String> headers = null;
-    public ArrayList<String> backToList = new ArrayList<>();
     private static final Log log = LogFactory.get();
+    public ArrayList<String> backToList = new ArrayList<>();
+    LinkedHashMap<String, String> headers = null;
     private String cookie;
 
     public Signin(String cookie) {
-        this.cookie=cookie.replace("Cookie: ", "").trim();
-    }
-
-    public ArrayList<String> getBackToList() {
-        return backToList;
+        this.cookie = cookie.replace("Cookie: ", "").trim();
     }
 
     public Signin() {
     }
+    public ArrayList<String> getBackToList() {
+        return backToList;
+    }
+
     /**
      * 将用户复制的请求头信息转换为linkedhashmap令程序可识别
      *
      * @param str 用户复制的请求头信息
      * @return 包含所有请求头信息的linkedhashmap
      */
-    public LinkedHashMap<String, String> resolveHeadsToMap(String str) {
+    public static LinkedHashMap<String, String> resolveHeadsToMap(String str) {
         String[] split = str.split("\\n");
         LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
         for (String s : split) {
@@ -89,7 +91,7 @@ public class Signin {
      * @param str 复制的post参数
      * @return 包含所有post参数的linkedhashmap
      */
-    public LinkedHashMap<String, String> resolveArgsToMap(String str) {
+    public static LinkedHashMap<String, String> resolveArgsToMap(String str) {
         String[] strings = str.split("&");
         LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
         for (String string : strings) {
@@ -112,12 +114,12 @@ public class Signin {
      * @param args    参数
      * @return 返回值
      */
-    public String post(String url, LinkedHashMap<String, String> headers, LinkedHashMap<String, String> args) {
+    public static String post(String url, LinkedHashMap<String, String> headers, LinkedHashMap<String, String> args) {
         try (CloseableHttpClient httpClient = HttpRequestUtils.getHttpClient()) {
             //第一步 配置 Post 请求 Url
             HttpPost httpPost = new HttpPost(url);
 //           第二步 配置请求头
-            if (Objects.nonNull(headers)){
+            if (Objects.nonNull(headers)) {
                 for (Map.Entry<String, String> entry : headers.entrySet()) {
                     httpPost.setHeader(entry.getKey(), entry.getValue());
                 }
@@ -155,7 +157,7 @@ public class Signin {
      * @param headers 头标识
      * @return 返回值
      */
-    public String get(String url, LinkedHashMap<String, String> headers) {
+    public static String get(String url, LinkedHashMap<String, String> headers) {
         try (CloseableHttpClient httpclient = HttpRequestUtils.getHttpClient()) {
             //第一步 配置 get 请求 Url
             HttpGet httpGet = new HttpGet(url);
@@ -183,78 +185,5 @@ public class Signin {
             e.printStackTrace();
         }
         return null;
-    }
-    public siginEnum getAllBa(String url) {
-        if (Objects.isNull(cookie)){
-            log.error("用户的Cookie为空，无法继续执行");
-            return siginEnum.SIGNIN_ERROR_COOKIE_IS_NULL;
-        }
-        if (Objects.isNull(url)){
-            url=TIEBA_SEED_URL;
-        }
-        log.info("开始获取所有贴吧");
-        headers = resolveHeadsToMap(
-                "Host: tieba.baidu.com\n" +
-                        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0\n" +
-                        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
-                        "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\n" +
-                        "Accept-Encoding: gzip, deflate, br\n" +
-                        "Connection: keep-alive\n" +
-                        "Cookie: "+ cookie +"\n"+
-                        "Upgrade-Insecure-Requests: 1\n" +
-                        "Cache-Control: max-age=0"
-        );
-        String s = get(url, headers);
-        Document doc = Jsoup.parse(s);
-        Elements links = doc.select("a:not(.like_badge)[title]");
-        if (links==null||links.isEmpty()){
-            log.error("获取到贴吧信息错误!,并未获取到任何吧!");
-            return siginEnum.SIGNIN_ERROR_TEIBA_IS_NULL;
-        }
-        for (Element link : links) {
-            log.info("找到贴吧:"+link.text());
-            baidutieba(link.text());
-        }
-        Elements select = doc.select(".pagination>a");
-        if (!Objects.equals(select.size(), 0) && Objects.equals(select.last().text(), "尾页")) {
-            Set<String> set = new HashSet<>();
-            select.forEach(e -> {
-                String href = e.attr("href");
-                set.add(href);
-            });
-            set.forEach(e -> getAllBa("http://tieba.baidu.com/" + e));
-        }
-        return siginEnum.SIGNIN_SUUCCESS;
-    }
-
-    public void baidutieba(String tablename) {
-        log.info("贴吧:"+tablename+"开始签到");
-        Document parse = Jsoup.parse(get("https://tieba.baidu.com/f?kw=" + tablename, headers));
-        Elements script = parse.getElementsByTag("script").eq(1);
-        String text = script.toString();
-        Matcher matcher = TBS.matcher(text);
-        String TBid = null;
-        if (matcher.find()) {
-            String group = matcher.group();
-            TBid = group.substring(group.indexOf("\"") + 1, group.lastIndexOf("\""));
-        } else {
-            log.info("发生错误");
-        }
-        LinkedHashMap<String, String> args = resolveArgsToMap(
-                "ie=utf-8&kw=" +
-                        tablename +
-                        "&tbs=" +
-                        TBid
-        );
-        String post = post("https://tieba.baidu.com/sign/add", headers, args);
-        JSONObject jsonObject = JSONUtil.parseObj(post);
-        Object no = jsonObject.get("no");
-        if (Objects.equals(no, 1101)) {
-            backToList.add(tablename + ",您今日已经签到");
-            log.info(tablename + ",您今日已经签到");
-        } else if (Objects.equals(no, 0)) {
-            log.info(tablename + ",签到成功");
-            backToList.add(tablename + ",签到成功");
-        }
     }
 }
--- src/main/java/com/rui/ok.java
+++ src/main/java/com/rui/ok.java
@@ -4,15 +4,36 @@
 
 package com.rui;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.lang.Console;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.CountDownLatch;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.rui.Signin.resolveArgsToMap;
 
 /**
  * @author 8
  */
 public class ok extends JFrame {
+    public static final Pattern TBS = Pattern.compile(".tbs.:\\s*\"\\S*");
+    public static final String TIEBA_SEED_URL = "http://tieba.baidu.com/f/like/mylike?pn=1";
+    LinkedHashMap<String, String> headers = null;
+    private String cookie;
     public ok() {
         Font f = new Font("宋体",Font.PLAIN,12);
         String[] names ={ "Label", "CheckBox", "PopupMenu","MenuItem", "CheckBoxMenuItem",
@@ -29,7 +50,7 @@ public class ok extends JFrame {
         initComponents();
 //        设置可见
         setVisible(true);
-        logTextArea.append("欢迎使用贴吧云签到GUI本地版.\n如果你需要源码或者云签到版本请前往\nhttps://github.com/Rui2450\n如果你需要使用帮助请前往");
+        logTextArea.append("欢迎使用贴吧云签到GUI本地版.\n如果你需要源码,或者云签到版本,或者有什么建议,再或者单纯的想夸夸我,请前往\n  https://www.52pojie.cn/thread-1107576-1-1.html  \n如果你需要使用教程请前往\n   https://www.52pojie.cn/thread-1107576-1-1.html  \n");
         //设置关闭方式 如果不设置的话 关闭窗口之后不会退出程序
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
     }
@@ -52,14 +73,10 @@ public class ok extends JFrame {
              */
             @Override
             protected String doInBackground() {
-                Signin signin = new Signin(cookiesText.getText());
-                siginEnum allBa = signin.getAllBa(null);
+                cookie=cookiesText.getText().replace("Cookie: ", "").trim();
+                siginEnum allBa = getAllBa(null);
                 logTextArea.append(allBa.getMessage()+"\n");
                 if (allBa.isStats()){
-                    ArrayList<String> backToList = signin.backToList;
-                    backToList.forEach(a->{
-                        logTextArea.append(a+"\n");
-                    });
                     start.setText("签到完成!");
                 }else {
                     logTextArea.append(allBa.getMessage()+"\n");
@@ -70,7 +87,83 @@ public class ok extends JFrame {
         };
         task.execute();
     }
+    public siginEnum getAllBa(String url) {
+        if (Objects.isNull(cookie)) {
+            logTextArea.append("用户的Cookie为空，无法继续执行\n");
+            return siginEnum.SIGNIN_ERROR_COOKIE_IS_NULL;
+        }
+        if (Objects.isNull(url)) {
+            url = TIEBA_SEED_URL;
+        }
+        headers = Signin.resolveHeadsToMap(
+                "Host: tieba.baidu.com\n" +
+                        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0\n" +
+                        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\n" +
+                        "Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\n" +
+                        "Accept-Encoding: gzip, deflate, br\n" +
+                        "Connection: keep-alive\n" +
+                        "Cookie: " + cookie + "\n" +
+                        "Upgrade-Insecure-Requests: 1\n" +
+                        "Cache-Control: max-age=0"
+        );
+        String s = Signin.get(url, headers);
+        Document doc = null;
+        if (s != null) {
+            doc = Jsoup.parse(s);
+        }else {
+            logTextArea.append("错误！并未获取到任何贴吧\n");
+        }
+        Elements links = doc.select("a:not(.like_badge)[title]");
+        if (links == null || links.isEmpty()) {
+            logTextArea.append("错误！并未获取到任何贴吧\n");
+            return siginEnum.SIGNIN_ERROR_TEIBA_IS_NULL;
+        }
+        for (Element link : links) {
+            ThreadUtil.execute(() -> {
+                baidutieba(link.text());
+            });
+        }
+        Elements select = doc.select(".pagination>a");
+        if (!Objects.equals(select.size(), 0) && Objects.equals(select.last().text(), "尾页")) {
+            Set<String> set = new HashSet<>();
+            select.forEach(e -> {
+                String href = e.attr("href");
+                set.add(href);
+            });
+            ThreadUtil.execute(() -> {
+                set.forEach(e -> getAllBa("http://tieba.baidu.com/" + e));
+            });
+        }
+        return siginEnum.SIGNIN_SUUCCESS;
+    }
 
+    public void baidutieba(String tablename) {
+        Document parse = Jsoup.parse(Signin.get("https://tieba.baidu.com/f?kw=" + tablename, headers));
+        Elements script = parse.getElementsByTag("script").eq(1);
+        String text = script.toString();
+        Matcher matcher = TBS.matcher(text);
+        String TBid = null;
+        if (matcher.find()) {
+            String group = matcher.group();
+            TBid = group.substring(group.indexOf("\"") + 1, group.lastIndexOf("\""));
+        } else {
+            logTextArea.append("发生错误");
+        }
+        LinkedHashMap<String, String> args = Signin.resolveArgsToMap(
+                "ie=utf-8&kw=" +
+                        tablename +
+                        "&tbs=" +
+                        TBid
+        );
+        String post = Signin.post("https://tieba.baidu.com/sign/add", headers, args);
+        JSONObject jsonObject = JSONUtil.parseObj(post);
+        Object no = jsonObject.get("no");
+        if (Objects.equals(no, 1101)) {
+            logTextArea.append(tablename + ",您今日已经签到\n");
+        } else if (Objects.equals(no, 0)) {
+            logTextArea.append(tablename + ",签到成功！\n");
+        }
+    }
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         panel1 = new JPanel();
@@ -83,6 +176,7 @@ public class ok extends JFrame {
 
         //======== this ========
         setTitle("\u8d34\u5427\u5168\u7b7e\u5230-52pojie");
+        setIconImage(new ImageIcon(getClass().getResource("/image/\u7b7e\u5230.png")).getImage());
         Container contentPane = getContentPane();
         contentPane.setLayout(null);
 
